8c6fbf4c7bfe560041611b5b83209ebd6c946bb3
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2024-04-08 23:01:48 +0300

    Exersize

---
+++ 5.breakPointTest_script .js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import {sleep} from'k6';
+
+export const options={
+   stages:[
+     {
+      duration: "2h",
+      target:   100000, 
+     }, 
+    
+   ]
+}
+
+export default function () {
+http.get('https://test.k6.io/news.php');
+sleep(1);
+
+}
+
---
+++ 6.checksTest_script.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import {sleep} from'k6';
+import {check} from'k6';
+
+
+
+export default function () {
+  const response = http.get('https://test.k6.io')
+  check (response, {
+    'HTTP status is 200': (r) =>r.status === 200,
+    'Homepage welocome header resent': 
+    (r)=>r.body.includes("Welcome to the k6.io demo site!")
+  })
+sleep(1);
+}
+
